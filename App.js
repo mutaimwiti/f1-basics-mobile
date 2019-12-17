99fba566c871605dadca79e2558aac01a39bad38
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-17 18:54:57 +0300

    (feat): give components and titles more meaningful names

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {View} from 'react-native';
 
+import Grid from "./src/components/Grid";
 import Title from "./src/components/Title";
-import F1Image from "./src/components/F1Image";
-import F12020Grid from "./src/components/F12020Grid";
+import F1Image from "./src/components/Photo";
 
 import {image, drivers} from "./src/utils/data";
 
@@ -11,10 +11,10 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Title data={'Nico Hulkenberg'}/>
+        <Title data={'Formula 1'}/>
         <F1Image image={image}/>
-        <Title data={'2019 Grid Drivers'}/>
-        <F12020Grid drivers={drivers}/>
+        <Title data={'2020 Grid'}/>
+        <Grid drivers={drivers}/>
       </View>
     )
   }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Text, FlatList, StyleSheet, View} from 'react-native';
 
-export default class F12020Grid extends React.Component {
+export default class Grid extends React.Component {
   render() {
     const {drivers} = this.props;
 
