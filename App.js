245da8723e30ab3466452841e3576a3763d69ddd
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-12-17 16:42:33 +0300

    (feat): add nico hulkenberg image

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    const image = {uri: 'https://www.formula1.com/content/dam/fom-website/manual/XPB_Images/XPB_1025389_HiRes.jpg.transform/9col/image.jpg'};
+
+    return (
+      <View>
+        <Text style={this.styles.title}>Nico Hulkenberg</Text>
+        <Image source={image} style={this.styles.image}/>
+      </View>
+    )
+  }
+
+  styles = StyleSheet.create({
+    title: {
+      marginTop: '5%',
+      textAlign: 'center',
+      fontWeight: 'bold',
+      fontSize: 20
+    },
+    image: {
+      marginLeft: '5%',
+      marginTop: '2%',
+      width: '90%',
+      aspectRatio: 1,
+      borderRadius: 10
+    }
+  });
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
